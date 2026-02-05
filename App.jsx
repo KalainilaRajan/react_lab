3585999f44dbe41924a792fe1e9265a135d24658
KalainilaRajan <kalainilarajan@gmail.com>
2026-02-05 10:36:09 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -4,21 +4,56 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("white")
+  const [activeTab, setactiveTab] = useState("Home")
 
   return (
     <>
-     <div>
-      <h1>Color-Picker</h1>
-      <button onClick={()=>setColor("red")}>Red</button>
-      <button onClick={()=>setColor("green")}>Green</button>
-      <button onClick={()=>setColor("blue")}>Blue</button>
+    <div style={StyleSheet.Container}>
+      <h2>Tab Component</h2>
+      <div style={Styles.tabContainer}>
+        <button style={activeTab==="Home" ? Styles.activeBtn: Styles.Btn}onClick={()=>setactiveTab("Home")}>Home</button>
+        <button style={activeTab==="About" ? Styles.activeBtn: Styles.Btn}onClick={()=>setactiveTab("About")}>About</button>
+        <button style={activeTab==="Content" ? Styles.activeBtn: Styles.Btn}onClick={()=>setactiveTab("Content")}>Content</button>
+        
+      </div>
 
-      <div className='bgchange'style={{backgroundColor : color}}></div>
-     </div>
-       
+      <div style={Styles.contentBox}>
+        {activeTab ==="Home" &&(
+          <p>This is home page</p>
+        )}
+
+        {activeTab==="About" &&(
+          <p>This is about page</p>
+        )}
+
+        {activeTab==="Content" &&(
+          <p>This is Content page </p>
+        )}
+      </div>
+
+    </div>
+      
     </>
   )
+}
+
+const Styles={
+  Container:{
+    minHeight:"100vh",
+    backgroundColor:"lightblue"
+  },
+
+  tabContainer:{
+    padding:"20px"
+  },
+
+  contentBox:{
+    height:"100px",
+    width :"300px",
+    border :"1px Solid",
+    borderRadius:"10px",
+    marginLeft:"59px"
+  }
 }
 
 export default App
